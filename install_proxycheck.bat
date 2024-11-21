57048cf305e64309f4e848e47cd3605b37f3ae59
Roxana <122395001+asroxy@users.noreply.github.com>
2024-11-21 12:22:32 -0500

    Update install_proxycheck.bat

--- install_proxycheck.bat
+++ install_proxycheck.bat
@@ -1,24 +1,19 @@
 @echo off
 setlocal
 
-REM Determine the OS type
 set "ARCH=x64"
 set "URL=https://github.com/asroxy/proxycheck-cli/releases/download/v1.01/proxycheck_windows.zip"
 set "TEMP_DIR=%TEMP%\proxycheck"
 
-REM Download the file
 echo Downloading Proxycheck for Windows...
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%TEMP_DIR%.zip')"
 
-REM Extract the ZIP file
 echo Extracting...
 powershell -Command "Expand-Archive -Path '%TEMP_DIR%.zip' -DestinationPath '%TEMP_DIR%'"
 
-REM Move to C:\Windows
 echo Installing to C:\Windows...
 move /Y "%TEMP_DIR%\proxycheck.exe" "C:\Windows\"
 
-REM Clean up
 echo Cleaning up...
 rd /s /q "%TEMP_DIR%"
 del "%TEMP_DIR%.zip"
